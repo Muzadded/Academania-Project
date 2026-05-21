57276317e482ff03b3d3617d85d8251ab2c3e591
Muzadded <alifchowdhury27@gmail.com>
2026-05-21 08:08:58 +0600

    fix prisma config

--- Backend/prisma.config.ts
+++ Backend/prisma.config.ts
@@ -3,11 +3,9 @@ import { defineConfig } from 'prisma/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
-  migrate: {
-    async adapter() {
-      return new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-    },
+  engine: 'js',
+  adapter: async () => {
+    return new PrismaPg({ connectionString: process.env.DATABASE_URL! });
   },
 });
